c87eceef8fd5cea15adaf154c18d443c1bab4237
Isaac <Isaac@lkedohkess.c.byui.edu>
2016-10-06 10:34:17 -0600

    10-6-2016 Built the player class

--- StrangerThings/src/strangerthings/StrangerThings.java
+++ StrangerThings/src/strangerthings/StrangerThings.java
@@ -5,6 +5,8 @@
  */
 package strangerthings;
 
+import project.strangerThings.model.Player;
+
 /**
  *
  * @author Isaac
@@ -15,7 +17,13 @@ public class StrangerThings {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       Player playerOne = new Player();
+       
+       playerOne.setName("Enock");
+       playerOne.setBestTime(7.00);
+       
+       String playerInfo = playerOne.toString();
+       System.out.println(playerInfo);
+    } 
     
 }
