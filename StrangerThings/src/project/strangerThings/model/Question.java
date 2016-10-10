dcd7bc5476643dea48d89cdea55ab8d2ea42792a
Yamisteven23 <Yamisteven23@Goku>
2016-10-10 09:18:02 -0600

    10-07-16 Finished Location and Qustion classes

--- StrangerThings/src/project/strangerThings/model/Question.java
+++ StrangerThings/src/project/strangerThings/model/Question.java
@@ -6,6 +6,7 @@
 package project.strangerThings.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -15,4 +16,74 @@ public class Question implements Serializable{
     private String question;
     private String answer;
     private String correctAnswer;
+
+    public Question() {
+    }
+    
+    
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.question);
+        hash = 97 * hash + Objects.hashCode(this.answer);
+        hash = 97 * hash + Objects.hashCode(this.correctAnswer);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Question other = (Question) obj;
+        if (!Objects.equals(this.question, other.question)) {
+            return false;
+        }
+        if (!Objects.equals(this.answer, other.answer)) {
+            return false;
+        }
+        if (!Objects.equals(this.correctAnswer, other.correctAnswer)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" + "question=" + question + ", answer=" + answer + ", correctAnswer=" + correctAnswer + '}';
+    }
+    
+    
+    
+    
 }
