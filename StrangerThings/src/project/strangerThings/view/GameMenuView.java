3dd01a00ef2ea5bc0888551bc1c30b1839625dd2
Yamisteven23 <Yamisteven23@Goku>
2016-10-31 09:39:21 -0600

    10-31-16 Started Game Menu

--- StrangerThings/src/project/strangerThings/view/GameMenuView.java
+++ StrangerThings/src/project/strangerThings/view/GameMenuView.java
@@ -5,6 +5,8 @@
  */
 package project.strangerThings.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Yamisteven23
@@ -12,7 +14,106 @@ package project.strangerThings.view;
 public class GameMenuView {
 
     void displayMenu() {
-        System.out.println("\n*** displayMenu() called ***");
+        private String menu;
+
+   public void displayGameMenuView() {
+        
+        boolean done = false;
+        do{
+        
+        String menuOption = this.getMenuOption();
+        if (menuOption.toUpperCase().equals("E"))
+            return;
+        
+        done = this.doAction(menuOption);
+        } while (!done);
     }
-    
-}
+
+    public GameMenuView() {
+        this.menu = "\n"
+                    +"\n----------------------------------"
+                    +"\n|         Game Menu              |"
+                    +"\n---------------------------------|"
+                    +"\nM- Move                          |"
+                    +"\nI- Inventory                     |"
+                    +"\nV- View Map                      |"
+                    +"\nS- Save Game                     |"
+                    +"\nH- Help Menu                     |"
+                    +"\nW- Manufacture Weapon            |"
+                    +"\nL- Look for items                |"
+                    +"\nE- Exit Game                     |"
+                    +"\n----------------------------------";
+    }
+
+   private String getMenuOption() {
+        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+        String value = ""; //value to be returned
+        boolean valid = false; //initalize to not valid
+        
+        while (!valid) {
+            System.out.println("\n" + this.menu);
+            
+            value = keyboard.nextLine(); //get next line typed by keyboard
+            value = value.trim(); //trim off leading and trailing whitespace
+            
+            if (value.length() < 1){ //value is blank
+                System.out.println("\nInvalid value; value cannot be blank");
+                continue;
+            }
+            break; //end the loop
+            
+        }
+        return value; //return the value entered
+    }
+
+    private boolean doAction(String menuOption) {
+       String choice = menuOption.toUpperCase();
+        
+        switch (choice){
+            case "M": //Move
+                this.move();
+                break;
+            case "I": //Inventory
+                this.checkInventory();
+                break;
+            case "V": //View Map
+                this.viewMap();
+                break;
+            case "S": //Save Game
+                this.checkInventory();
+                break;
+            case "W": //Make Weapon
+                this.manufactureWeapon();
+                break;
+            case "L": //Look for items
+                this.checkInventory();
+                break;
+            case "H": //Help Menu
+                this.helpMenu();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Please Try again");
+                break;
+        }
+        return false;
+    }
+
+    private void move() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void checkInventory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void viewMap() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void manufactureWeapon() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void helpMenu() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
