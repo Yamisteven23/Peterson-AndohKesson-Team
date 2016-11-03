8a4f7c34888f4ceb907618005ab87eb893c95b94
Yamisteven23 <Yamisteven23@Goku>
2016-11-03 09:09:14 -0600

    11-2-16 Working on TheShackView

---
+++ StrangerThings/src/project/strangerThings/view/TheNoteView.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.strangerThings.view;
+
+import java.util.Random;
+import java.util.Scanner;
+import project.strangerThings.control.MapControl;
+
+/**
+ *
+ * @author Yamisteven23
+ */
+public class TheNoteView {
+
+    private String displayMessage;
+    private long rn;
+    private long rn2;
+
+    public TheNoteView() {
+        Random randomGenerator = new Random();
+        this.rn2 = (long) randomGenerator.nextInt(11);
+        this.rn = (long) randomGenerator.nextInt(1979) + 22;
+
+        this.displayMessage
+                = "\n*****************************************************************"
+                + "\n*                                                               *"
+                + "\n  A man has " + this.rn + " apples. He gives half of them to his mom   "
+                + "\n  and then gives " + this.rn2 + " to his brother. How many apples does "
+                + "\n  he have left?                                                 "
+                + "\n*                                                               *"
+                + "\n*****************************************************************";
+
+    }
+
+    private String getAnswer() {
+        Scanner keyboard = new Scanner(System.in); //get infile for keyboard
+        String value = ""; //value to be returned
+        boolean valid = false; //initalize to not valid
+
+        while (!valid) {
+            System.out.println("\n" + this.displayMessage);
+
+            value = keyboard.nextLine(); //get next line typed by keyboard
+            value = value.trim(); //trim off leading and trailing whitespace
+
+            if (value.length() < 1) { //value is blank
+                System.out.println("\nInvalid value; value cannot be blank");
+                continue;
+            }
+            break; //end the loop
+
+        }
+        return value; //return the value entered
+    }
+
+    void displayNoteView() {
+        
+        boolean done = false;
+        do{
+        
+        String userAnswer = this.getAnswer();
+        if (userAnswer.toUpperCase().equals("E"))
+            return;
+        
+        done = this.doAction(userAnswer);
+        } while (!done);
+    }
+
+    private boolean doAction(String userAnswer) {
+        long answer = Long.parseLong(userAnswer);
+      long result = MapControl.openTheShack(this.rn, this.rn2,answer);
+      
+      if (result == 0){
+      
+      }
+       
+    return true;
+    }
+
+
+}
